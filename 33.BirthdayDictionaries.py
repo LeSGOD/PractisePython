501b1474e4449497091c2f16a0707e1e7ec794e8
Artur Leszczynski <artles00@wp.pl>
2020-06-12 21:37:01 +0200

    Birthday exercises

---
+++ 33.BirthdayDictionaries.py
@@ -0,0 +1,23 @@
+birthdays = {
+    'Albert Einstein': '03/14/1879',
+    'Benjamin Franklin': '01/17/1706',
+    'Ada Lovelace': '12/10/1815',
+    'Donald Trump': '06/14/1946',
+    'Rowan Atkinson': '01/6/1955'}
+
+
+def get_birthday_date():
+    print("Welcome to the birthday dictionary. We know the birthdays of: ")
+    for name in birthdays:
+        print(name)
+
+    print("Who's birthday do you want to look up?")
+    name = input()
+
+    if name in birthdays:
+        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+
+
+get_birthday_date()
