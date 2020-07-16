864b6d35ef19710758e9e1c066284b8365614cbe
Artur Leszczynski <artles00@wp.pl>
2020-07-16 11:07:07 +0200

    100 challange

---
+++ 100+ Python challenging programming exercises/Level_3/Q21.py
@@ -0,0 +1,25 @@
+"""
+A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps. The trace of robot movement is shown as the following:
+UP 5
+DOWN 3
+LEFT 3
+RIGHT 2
+¡­
+The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
+Example:
+If the following tuples are given as input to the program:
+UP 5
+DOWN 3
+LEFT 3
+RIGHT 2
+Then, the output of the program should be:
+2
+"""
+
+from math import sqrt
+
+
+def get_robot_position(UP, DOWN, LEFT, RIGHT):
+    return int(round((sqrt(abs(UP-DOWN)**2 + abs(LEFT-RIGHT)**2))))
+
+print (get_robot_position(5,3,3,2))
