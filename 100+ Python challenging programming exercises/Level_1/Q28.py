d70dabf72e8fbba3288c038e44e3d60e97efea13
Artur Leszczynski <artles00@wp.pl>
2020-07-20 18:25:48 +0200

    100 challange

---
+++ 100+ Python challenging programming exercises/Level_1/Q28.py
@@ -0,0 +1,7 @@
+"""
+Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+"""
+
+
+def sum_of_two_strings(number1, number2):
+    return int(number1) + int(number2)
