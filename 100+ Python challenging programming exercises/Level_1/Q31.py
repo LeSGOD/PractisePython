83dcb9224c5b898fe3e3e4dd4581fc5d8d836e94
Artur Leszczynski <artles00@wp.pl>
2020-07-21 21:24:51 +0200

    100 challange

---
+++ 100+ Python challenging programming exercises/Level_1/Q31.py
@@ -0,0 +1,12 @@
+"""
+Question:
+Define a function that can accept an integer number as input and print the "It is an even number" if the number is even,\
+otherwise print "It is an odd number".
+"""
+
+
+def odd_or_even(num):
+    if num % 2 == 0:
+        print("It is an even number")
+    else:
+        print("It is an odd number")
