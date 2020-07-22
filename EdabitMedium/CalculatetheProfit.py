4ba4d50209188aac9ad15defed65c60b256ba7a8
Artur Leszczynski <artles00@wp.pl>
2020-07-22 23:24:41 +0200

    edabit

---
+++ EdabitMedium/CalculatetheProfit.py
@@ -0,0 +1,9 @@
+"""
+You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. 
+You are given a dictionary containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory. 
+Return the total profit made, rounded to the nearest dollar. Assume all of the inventory has been sold.
+"""
+
+
+def profit(info):
+    return round((info["sell_price"] - info["cost_price"]) * info["inventory"])
