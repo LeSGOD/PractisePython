c3a8e84d6542f401cc56125ddcc096d558eec4d1
Artur Leszczynski <artles00@wp.pl>
2020-07-24 14:01:46 +0200

    edabit

---
+++ EdabitMedium/FormatIXUnpackingLists.py
@@ -0,0 +1,17 @@
+"""
+For each challenge of this series you do not need to submit a function. Instead, you need to submit a template string that can be formatted in order to get a certain outcome.
+
+Write three lists and a template string according to the following example. Notice the keyword argument elem:
+"""
+
+lst1 = ["John", "Joe", "Jack"]
+lst2 = ["E", "da", "bit"]
+lst3 = ["Metapod", "Magikarp", "Unown"]
+
+template = "My {elem} are: {}, {} and {}."
+
+# "My friends are: John, Joe and Jack."
+print(template.format(*lst1, elem="friends"))
+print(template.format(*lst2, elem="loves"))  # "My loves are: E, da and bit."
+# "My pokemon are: Metapod, Magikarp and Unown."
+print(template.format(*lst3, elem="pokemon"))
