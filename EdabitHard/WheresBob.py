874f5238b24df6fb0bd6ad0cd231b49c1a29b876
LeSGOD <artles00@wp.pl>
2020-07-25 10:14:11 +0200

    edabit

---
+++ EdabitHard/WheresBob.py
@@ -0,0 +1,14 @@
+    """Write a function that searches a list of names (unsorted) for the name "Bob" and returns the location in the list. 
+    If Bob is not in the array, return -1.
+    """
+
+
+def find_bob(names):
+    
+    if "Bob" in names:
+        return names.index("Bob")
+        
+    else:
+        return -1 
+
+#find_bob(["Jimmy", "Layla", "Bob"]) ➞ 2
