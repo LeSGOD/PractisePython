3f933f08e108ed4dc1ad7195d9e0b8985ad77972
Artur Leszczynski <artles00@wp.pl>
2020-07-26 18:26:32 +0200

    edabitHard

---
+++ EdabitHard/Fridaythe13th.py
@@ -0,0 +1,9 @@
+"""
+Given the month and year as numbers, return whether that month contains a Friday 13th.
+"""
+
+
+import datetime
+def has_friday_13(month, year):
+	x = datetime.datetime(year, month, 13)
+	return x.strftime("%A") == "Friday"
