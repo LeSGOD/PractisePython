eb47e7cc4230b33d8b69168396631a33a72279b9
Artur Leszczynski <artles00@wp.pl>
2020-07-27 22:47:46 +0200

    edabiMedium,

---
+++ EdabitMedium/Ones,ThreesandNines(Version #1).py
@@ -0,0 +1,11 @@
+"""
+Given an int, figure out how many ones, threes and nines you could fit into the number. You must create a class. You will make variables(self.ones, self.threes, self.nines) to do this
+"""
+
+
+class ones_threes_nines:
+
+    def __init__(self, n):
+        self.ones = n
+        self.threes = n // 3
+        self.nines = n // 9
