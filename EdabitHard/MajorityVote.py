2439d30c98de0ca51dbb0c26a899b926c0b175ee
Artur Leszczynski <artles00@wp.pl>
2020-07-29 23:14:50 +0200

    edabitHard

---
+++ EdabitHard/MajorityVote.py
@@ -0,0 +1,17 @@
+"""
+Create a function that returns the majority vote in a list. A majority vote is an element that occurs > N/2 times in a list (where N is the length of the list).
+
+Examples
+majority_vote(["A", "A", "B"]) ➞ "A"
+
+majority_vote(["A", "A", "A", "B", "C", "A"]) ➞ "A"
+
+majority_vote(["A", "B", "B", "A", "C", "C"]) ➞ None
+"""
+
+
+def majority_vote(lst):
+    for i in set(lst):
+        if lst.count(i) > len(lst)//2:
+            return i
+    return None
