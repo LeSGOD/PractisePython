2f3c2e71d8c8a4257f51aea92b609bec2bd9575d
LeSGOD <artles00@wp.pl>
2020-08-05 16:12:43 +0200

    edabitHard

---
+++ EdabitHard/LayersinaRug.py
@@ -0,0 +1,35 @@
+"""
+Write a function that counts how many concentric layers a rug.
+
+Examples
+count_layers([
+  "AAAA",
+  "ABBA",
+  "AAAA"
+]) ➞ 2
+
+count_layers([
+  "AAAAAAAAA",
+  "ABBBBBBBA",
+  "ABBAAABBA",
+  "ABBBBBBBA",
+  "AAAAAAAAA"
+]) ➞ 3
+
+count_layers([
+  "AAAAAAAAAAA",
+  "AABBBBBBBAA",
+  "AABCCCCCBAA",
+  "AABCAAACBAA",
+  "AABCADACBAA",
+  "AABCAAACBAA",
+  "AABCCCCCBAA",
+  "AABBBBBBBAA",
+  "AAAAAAAAAAA"
+]) ➞ 5
+
+"""
+
+def count_layers(rug):
+	return len(set(rug))
+
